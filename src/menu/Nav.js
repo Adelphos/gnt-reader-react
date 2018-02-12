4bb84928cfab0f926a7b80850ec80fa421e1ee5c
Brian Knapp <brian@brianknappdeveloper.com>
2018-02-11 19:22:08 -0800

    removed unneeded dependencies

--- src/menu/Nav.js
+++ src/menu/Nav.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import * as Icons from '../util/Icons.js'
 
 class Nav extends Component {
   render() {
     return (
-      <nav className="header-icons">
-      </nav>
+      <nav className="header-icons"></nav>
     );
   }
 };
